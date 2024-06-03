0a9c6db7c74835b2367dc3ce13478780cf420c39
SacredEP <mecnick@pm.me>
2024-06-03 10:17:28 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,5 @@
-// Placeholder for JavaScript code
+function addToCart(product) {
+    alert(product + " has been added to your cart!");
+    // Here you could add functionality to actually add the product to a shopping cart
+    // This could involve storing the cart data in local storage or sending it to a server
+}
